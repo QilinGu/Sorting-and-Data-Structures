a5720fdfad96a24fd19da12239cd125e2c4d282d
Ethan Zimbra <ezimbra@outlook.com>
2016-09-02 15:44:07 -0700

    Finished max pairwise product problem

--- Algorithmic-Toolbox/Week-1/max_pairwise_product.py
+++ Algorithmic-Toolbox/Week-1/max_pairwise_product.py
@@ -7,27 +7,38 @@ Find the max pairwise product
 import sys
 
 def main():
-    """
-    Main entry point for the script if file is executed directly
-    """
-    input = sys.stdin.read()
-    tokens = input.split()
+	"""
+	Main entry point for the script if file is executed directly
+	"""
+	lines = sys.stdin.read().splitlines()
+	for line in lines:
+		tokens = line.split()
+		tokens = [int(x) for x in tokens]
+		if len(tokens) > 1:
+			print(max_pairwise_product(tokens))
 
 def max_pairwise_product(nums):
-    """
-    Calculate the max product of two numbers in the provided list nums,
+	"""
+	Calculate the max product of two numbers in the provided list nums,
 
-    Args:
-        num (list): list of integers
+	Args:
+		num (list): list of integers
 
-    Returns:
-        integer: the computed max pair product from the provided list num
-    """
-    pass
+	Returns:
+		integer: the computed max pair product from the provided list num
+	"""
+	if len(nums) < 2:
+		return int(nums[0])
+	
+	max1 = max(nums)
+	nums.remove(max1)
+	max2 = max(nums)
+	
+	return max1 * max2
 
 def stress_test(good_funct, test_funct):
-    pass
+	pass
 
 if __name__ == "__main__":
-    main()
-    
+	main()
+	
--- Algorithmic-Toolbox/Week-1/max_pairwise_product_ut.py
+++ Algorithmic-Toolbox/Week-1/max_pairwise_product_ut.py
@@ -4,13 +4,21 @@ Unit test suite for
 
 @author Ethan Zimbra
 """
-import max_pairwise_product
+import max_pairwise_product as mp
 import unittest
 
 class TestMaxPairwiseProduct(unittest.TestCase):
-    def 
+
+	def test_two(self):
+		self.assertEqual(mp.max_pairwise_product([4, 7]), 28)
+		
+	def test_multiple(self):
+		self.assertEqual(mp.max_pairwise_product([2, 1, 6, 8]), 48)
+	
+	def test_same(self):
+		self.assertEqual(mp.max_pairwise_product([2, 1, 5, 5]), 25)
 
 
 if __name__ == "__main__":
-    main()
-    
+	unittest.main()
+	
