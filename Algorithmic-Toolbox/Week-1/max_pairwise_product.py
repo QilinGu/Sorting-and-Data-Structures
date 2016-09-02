dd60e74f8ddb7532054554791d90ca3bc5248465
Ethan Zimbra <ezimbra@outlook.com>
2016-09-01 17:08:34 -0700

    Added max pairwise product initial files

---
+++ Algorithmic-Toolbox/Week-1/max_pairwise_product.py
@@ -0,0 +1,33 @@
+#! python3
+"""
+Find the max pairwise product
+
+@author Ethan Zimbra
+"""
+import sys
+
+def main():
+    """
+    Main entry point for the script if file is executed directly
+    """
+    input = sys.stdin.read()
+    tokens = input.split()
+
+def max_pairwise_product(nums):
+    """
+    Calculate the max product of two numbers in the provided list nums,
+
+    Args:
+        num (list): list of integers
+
+    Returns:
+        integer: the computed max pair product from the provided list num
+    """
+    pass
+
+def stress_test(good_funct, test_funct):
+    pass
+
+if __name__ == "__main__":
+    main()
+    
---
+++ Algorithmic-Toolbox/Week-1/max_pairwise_product_ut.py
@@ -0,0 +1,16 @@
+#! python3
+"""
+Unit test suite for
+
+@author Ethan Zimbra
+"""
+import max_pairwise_product
+import unittest
+
+class TestMaxPairwiseProduct(unittest.TestCase):
+    def 
+
+
+if __name__ == "__main__":
+    main()
+    
