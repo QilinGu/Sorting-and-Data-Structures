592b8b25b2024327744085921d44d0f7e64d7116
Ethan Zimbra <ezimbra@outlook.com>
2016-09-22 13:47:00 -0700

    Added fractional knapsack solution and updated unit tests

--- Algorithmic-Toolbox/Week-3/fractional_knapsack.py
+++ Algorithmic-Toolbox/Week-3/fractional_knapsack.py
@@ -1,17 +1,39 @@
-# Uses python3
+#! python3
 import sys
 
+
 def get_optimal_value(capacity, weights, values):
+
+    if len(weights) != len(values):
+        return -1.
+
+    # compute value per weight
     value = 0.
-    # write your code here
+    density = []
+    for x in range(len(weights)):
+        density.append(values[x] / weights[x])
+
+    pairing = sorted(list(zip(density, weights)), reverse=True)
+
+    # Compute max loot with at most one of each full item
+    for item in pairing:
+        if item[1] >= capacity:
+            value += capacity * item[0]
+            break
+        value += item[1] * item[0]
+        capacity -= item[1]
 
     return value
 
 
-if __name__ == "__main__":
+def main():
     data = list(map(int, sys.stdin.read().split()))
     n, capacity = data[0:2]
     values = data[2:(2 * n + 2):2]
     weights = data[3:(2 * n + 2):2]
     opt_value = get_optimal_value(capacity, weights, values)
     print("{:.10f}".format(opt_value))
+
+if __name__ == "__main__":
+    main()
+
--- Algorithmic-Toolbox/Week-3/unit_tests.py
+++ Algorithmic-Toolbox/Week-3/unit_tests.py
@@ -1,5 +1,6 @@
 import unittest
 from change import get_change
+from fractional_knapsack import get_optimal_value
 
 
 class TestChange(unittest.TestCase):
@@ -20,6 +21,17 @@ class TestChange(unittest.TestCase):
         self.assertEqual(get_change(16), 3)
         self.assertEqual(get_change(19), 6)
         self.assertEqual(get_change(28), 6)
+
+
+class TestKnapsack(unittest.TestCase):
+    def test_type(self):
+        self.assertTrue(type(get_optimal_value(10, [30], [500])).__name__ == "float")
+
+    def test_single(self):
+        self.assertEqual(round(get_optimal_value(10, [30], [500]), 4), 166.6667)
+
+    def test_multiple(self):
+        self.assertEqual(round(get_optimal_value(50, [20, 50, 30], [60, 100, 120]), 4), 180.0000)
 
 if __name__ == "__main__":
     unittest.main()
